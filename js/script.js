ec29082899c14dcd7c261dc1de805ecdd38a0e81
Filippo Bristot <filippo.bristot@hotmail.it>
2021-11-01 12:54:40 +0100

    refactor: airbnb rules

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-const dataURI = "./js/data.json";
+const dataURI = './js/data.json';
 
 async function data() {
   const resp = await fetch(dataURI);
   const data = await resp.json();
   const titleJ = data[1].title;
-  const index = data.filter((el) => el.title === "Social");
+  const index = data.filter((el) => el.title === 'Social');
   console.log(index[0].timeframes);
   //   console.log(titleJ + "J");
 }
 
 data();
 
-const timeCards = document.getElementsByClassName("time-card");
+const timeCards = document.getElementsByClassName('time-card');
 
 function getCategory(el) {
-  return el.getAttribute("data-category");
+  return el.getAttribute('data-category');
 }
 
 getCategory(timeCards[0]);
