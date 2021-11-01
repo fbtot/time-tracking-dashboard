0d93b11c97376c5f31e236cd698113a95be3e543
Filippo Bristot <filippo.bristot@hotmail.it>
2021-11-01 10:39:48 +0100

    feat: add DOM elements and update function

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
+/* ========================== § DOM ELEMENTS === */
+
+// Cards
+const timeCardsEl = document.getElementsByClassName("time-card");
+let getTimeElText = (el) => el.querySelector(".time-card__time");
+let getTimePrevElText = (el) => el.querySelector(".time-card__prev");
+const getCategory = (el) => el.getAttribute("data-category");
+
+// Menu
+const timeToggleMenuEl = document.getElementById("time-toggle-menu");
+const timeToggleLinkEl = timeToggleMenuEl.querySelectorAll(".header__time-toggle__link");
+const currentTimeframe = () => timeToggleMenuEl.querySelector(".active").getAttribute("data-timeframe");
+
+// url
 const dataURI = "./js/data.json";
 
+/* ========================== § GETTING DATA FROM JSON === */
 async function data() {
   const resp = await fetch(dataURI);
   const data = await resp.json();
-  const titleJ = data[1].title;
+  const titleJ = data["weekly"];
   const index = data.filter((el) => el.title === "Social");
-  console.log(index[0].timeframes);
+  console.log(data);
   //   console.log(titleJ + "J");
 }
 
 data();
 
-const timeCards = document.getElementsByClassName("time-card");
-
-function getCategory(el) {
-  return el.getAttribute("data-category");
+/* ========================== § OPDATE FUNCTION === */
+function updateTime(el) {
+  return (getTimeElText(el).innerText = "60hrs");
 }
 
-getCategory(timeCards[0]);
+function update() {
+  for (let i = 0; i < timeCardsEl.length; i++) {
+    const card = timeCardsEl[i];
+    updateTime(card);
+  }
+}
